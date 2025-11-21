22ff4e4c2ce1be73b0bc338fdf721bb80bf30566
Alex James <apjames93@gmail.com>
2025-11-20 19:55:24 -0700

    feat(tailyew): scatter plot chart add resize, legend position and tooltip

--- frontend/src/templates/demos/charts/scatter_plot_demo_section.rs
+++ frontend/src/templates/demos/charts/scatter_plot_demo_section.rs
@@ -12,18 +12,24 @@ pub fn scatter_plot_demo_section() -> Html {
     let props_table = vec![
         Column {
             header: "Prop".into(),
-            values: vec!["points".into()],
+            values: vec![
+                "data".into(),
+                "legend_position".into(),
+                ],
         },
         Column {
             header: "Type".into(),
-            values: vec!["Vec<ScatterPlotPoint>".into()],
+            values: vec![
+                "Vec<BarChartData>".into(),
+                "LegendPosition".into(),
+                ],
         },
         Column {
             header: "Description".into(),
             values: vec![
-                "List of points with `x`, `y`, and `color` used to render dots on the chart."
-                    .into(),
-            ],
+                "Vector of bar data containing label, value, and color.".into(),
+                "Where to place the legend relative to the chart. Ex: Left, Right, Top, Bottom, Auto. Auto places the legend above the chart on very small screens, and to the right on sm+ screens and is the default".into(),
+                ],
         },
     ];
 
